d2dcd68350a04b1a36e1ec4cc7b882a9ba363a73
Wilfried Roset <wilfriedroset@users.noreply.github.com>
2021-04-29 14:22:14 +0200

    [terraform-ovh-linux_host] Include domain_zone_record in output
    Signed-off-by: Wilfried Roset <wilfriedroset@users.noreply.github.com>

--- modules/terraform-ovh-linux_host/output.tf
+++ modules/terraform-ovh-linux_host/output.tf
@@ -1,3 +1,7 @@
 output "linuxhost" {
   value = openstack_compute_instance_v2.linuxhost.network[0].fixed_ip_v4
 }
+
+output "domain_zone_record" {
+  value = var.domain_name != "" ? "${ovh_domain_zone_record.linuxhost[0].subdomain}.${ovh_domain_zone_record.linuxhost[0].zone}" : ""
+}
