c9d3c006b5c2e194cfd3ceed75ea2d10add15b2d
Arnaud SINAYS <arnaud.sinays@ovhcloud.com>
2021-05-28 16:55:55 +0200

    m3db bench in progress
    added configuration to have:
      * m3db nodes
      * m3coordinator(s)
      * load balancers in front of the coordinators
      * m3query
      * m3aggregator
      * prometheus configuration to write in the m3backend

--- m3db/variables.tf
+++ m3db/variables.tf
@@ -1,13 +1,31 @@
 variable "m3_dbnode_count" {
   description = "The number of m3 dbnode instance."
   type        = number
-  default     = 3
+  default     = 6
 }
 
 variable "m3_coordinator_count" {
   description = "The number of m3 coordinator instance."
   type        = number
+  default     = 3
+}
+
+variable "m3_query_count" {
+  description = "The number of m3 query instance."
+  type        = number
   default     = 1
+}
+
+variable "m3_aggregator_count" {
+  description = "The number of m3 aggregator instance."
+  type        = number
+  default     = 3
+}
+
+variable "m3_lb_count" {
+  description = "The number of m3 lb instance."
+  type        = number
+  default     = 2
 }
 
 variable "m3_dbnode_instance_flavor" {
@@ -18,6 +36,24 @@ variable "m3_dbnode_instance_flavor" {
 
 variable "m3_coordinator_instance_flavor" {
   description = "The name of the instance to use for the m3_coordinator servers."
+  type        = string
+  default     = "s1-2"
+}
+
+variable "m3_query_instance_flavor" {
+  description = "The name of the instance to use for the m3_query servers."
+  type        = string
+  default     = "s1-2"
+}
+
+variable "m3_aggregator_instance_flavor" {
+  description = "The name of the instance to use for the m3_aggregator servers."
+  type        = string
+  default     = "s1-2"
+}
+
+variable "m3_lb_instance_flavor" {
+  description = "The name of the instance to use for the m3_lb servers."
   type        = string
   default     = "s1-2"
 }
---
+++ modules/terraform-ovh-m3db/lb.tf
@@ -0,0 +1,19 @@
+module "m3db_lb" {
+  source = "../terraform-ovh-linux_host"
+
+  count           = var.lb_count
+  instance_name   = format(var.lb_instance_name_template, count.index + 1)
+  instance_image  = var.instance_image
+  instance_flavor = var.lb_instance_flavor
+  ssh             = var.ssh
+  userdata_path   = var.userdata_path
+  security_groups = var.lb_security_groups
+
+  instance_metadata = {
+    role    = "lb"
+    subrole = "m3coordinator_lb"
+  }
+
+  private_network = var.private_network
+  domain_name     = var.domain_name
+}
