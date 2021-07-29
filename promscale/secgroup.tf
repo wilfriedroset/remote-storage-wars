c02f0cefa0784f5ab2c85015001215228f5fc07d
Wilfried Roset <wilfriedroset@users.noreply.github.com>
2021-07-29 18:51:08 +0200

    [secgroup][promscale] Open port range 8080-8090 for pprof
    Signed-off-by: Wilfried Roset <wilfriedroset@users.noreply.github.com>

--- promscale/secgroup.tf
+++ promscale/secgroup.tf
@@ -9,7 +9,7 @@ resource "openstack_networking_secgroup_rule_v2" "pprof" {
   ethertype         = "IPv4"
   protocol          = "tcp"
   port_range_min    = 8080
-  port_range_max    = 8080
+  port_range_max    = 8090
   remote_ip_prefix  = each.value
   security_group_id = openstack_networking_secgroup_v2.pprof_ui_security_group.id
 }
